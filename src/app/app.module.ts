0e1d2138033f25b22bb61d97af5aa7ed8337696c
Mariana Bosanska <bosanska.mariana@gmail.com>
2019-06-20 12:18:11 +0200

    add dynamic title on event select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EventSelectBoxComponent } from './event-select-box/event-select-box.component';
 import { AttributeSelectBoxComponent } from './attribute-select-box/attribute-select-box.component';
@@ -15,7 +15,8 @@ import { OperatorSelectBoxComponent } from './operator-select-box/operator-selec
   ],
   entryComponents: [EventSelectBoxComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
