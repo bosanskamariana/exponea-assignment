c58f5c647e14c2ebcdaadaf9efcc3c5dd08442f2
Mariana Bosanska <bosanska.mariana@gmail.com>
2019-07-02 09:42:45 +0200

    basic functionality for selecting operator

--- src/app/operator-select-box/operator-select-box.component.ts
+++ src/app/operator-select-box/operator-select-box.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Attribute, Operator } from '../data-model/types';
+import { numberOperators, stringOperators } from '../data-model/operators';
 
 @Component({
   selector: 'app-operator-select-box',
@@ -9,7 +11,37 @@ export class OperatorSelectBoxComponent implements OnInit {
 
   constructor() { }
 
+  operators = numberOperators;
+  selectedOperator = 'equals'; // default
+  opened = false;
+
+  @Input() attribute: Attribute;
+
   ngOnInit() {
   }
 
+  toggleSelect() {
+    this.opened = !this.opened;
+  }
+
+
+  setOperator(operator: Operator) {
+    console.log('operator', operator);
+    if (this.attribute.value2 && operator !== 'in between') {
+      this.attribute.value2 = '';
+    }
+    this.attribute.operator = operator;
+    this.selectedOperator = operator;
+    this.opened = false;
+  }
+
+  setNumOperators() {
+    this.attribute.type = 'number';
+    this.operators = numberOperators;
+  }
+
+  setStringOperators() {
+    this.attribute.type = 'text';
+    this.operators = stringOperators;
+  }
 }
