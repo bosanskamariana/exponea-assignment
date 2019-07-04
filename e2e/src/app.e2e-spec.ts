de7decca4542e3fe876ede7dffff0b73aa76f0d5
Mariana Bosanska <bosanska.mariana@gmail.com>
2019-07-04 23:45:43 +0200

    fix some tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to exponea-assignment!');
+    expect(page.getTitleText()).toEqual('CUSTOMER FILTER');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'exponea-assignment'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('exponea-assignment');
-  });
-
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to exponea-assignment!');
+    expect(compiled.querySelector('h1').textContent).toContain('CUSTOM FILTER');
   });
 });
