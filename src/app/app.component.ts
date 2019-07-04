9266a373b46fb2795839668a5fcd7d34050eac8e
Mariana Bosanska <bosanska.mariana@gmail.com>
2019-07-04 15:34:13 +0200

    example inital filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,20 @@ export class AppComponent implements OnInit {
   constructor(private filter: CustomFilterService) { }
 
   ngOnInit() {
-    // todo load init filter
+    this.filter.events = [{
+      attributes: [{ id: '_jxmyt5i2i8gfgrbmo', type: 'text', name: 'number of items', operator: 'grater than', value: 'ttttt', value2: '' },
+      { id: '_jxmytbw17i6qao8la', type: 'text', name: 'timestamp', operator: null, value: '', value2: '' },
+      { id: '_jxmytfkoq93uwfvtq', type: 'number', name: 'index', operator: 'in between', value: '4', value2: '8' }
+      ], name: 'python_script_event', id: '_jxlz0iuwybqxouwrf'
+    },
+    { attributes: [], name: 'python_script_event2', id: '_jxlz0iuwybqxouwgf' },
+    {
+      attributes: [{ id: '_jxmyt5i2i8gfgrbmo', type: 'text', name: 'number of items', operator: 'grater than', value: 'ttttt', value2: '' },
+      { id: '_jxmytbw17i6qao8la', type: 'text', name: 'timestamp', operator: null, value: '', value2: '' },
+      { id: '_jxmytfkoq93uwfvtq', type: 'number', name: 'index', operator: 'in between', value: '4', value2: '8' }
+      ], name: 'python_script_event', id: '_jxlz0iuwybqxouwrf'
+    },
+    { attributes: [], name: 'python_script_event2', id: '_jxlz0iuwybqxouwgf' }];
   }
 
   addFunnelStep(name: string = events[0]) {
