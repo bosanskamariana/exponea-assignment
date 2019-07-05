fb79e8b6f60c5dcecf2c767916fb4e59c0648e33
Mariana Bosanska <bosanska.mariana@gmail.com>
2019-07-05 07:40:06 +0200

    move blur function on attribute selectbox

--- src/app/operator-select-box/operator-select-box.component.ts
+++ src/app/operator-select-box/operator-select-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Attribute, Operator } from '../data-model/types';
 import { numberOperators, stringOperators } from '../data-model/operators';
 
